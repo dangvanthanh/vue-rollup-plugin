7b45642ba16dbd13e0ce0ea3e55fe87933557898
Dang Van Thanh <dangvanthanh@dangthanh.org>
2020-11-06 20:53:02 +0700

    Move prettier to package.json

--- build/rollup.config.base.js
+++ build/rollup.config.base.js
@@ -1,13 +1,11 @@
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import filesize from 'rollup-plugin-filesize';
 
 export default {
   input: 'src/index.js',
   plugins: [
-    resolve({
-      mainFields: ['main', 'browser', 'jsnext'],
-    }),
+    nodeResolve(),
     commonjs(),
     filesize(),
   ],
